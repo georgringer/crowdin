c8c25d2a491687e17b04c93e842af5abafa8016d
80Quattro <rr.jania@gmail.com>
2023-10-17 20:08:45 +0200

    [TASK] Remove typo3_mode BE

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,18 +3,16 @@
 defined('TYPO3') or exit();
 
 $boot = static function () {
-    if (TYPO3_MODE === 'BE') {
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-postProcess']['crowdin-inline-translation']
-            = \GeorgRinger\Crowdin\Hooks\PageRendererHook::class.'->run';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-postProcess']['crowdin-inline-translation']
+        = \GeorgRinger\Crowdin\Hooks\PageRendererHook::class.'->run';
 
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Localization\LanguageService::class] = [
-            'className' => \GeorgRinger\Crowdin\Xclass\LanguageServiceXclassed::class,
-        ];
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Localization\LanguageService::class] = [
+        'className' => \GeorgRinger\Crowdin\Xclass\LanguageServiceXclassed::class,
+    ];
 
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Localization\LanguageServiceFactory::class] = [
-            'className' => \GeorgRinger\Crowdin\Xclass\LanguageServiceFactoryXclassed::class,
-        ];
-    }
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Localization\LanguageServiceFactory::class] = [
+        'className' => \GeorgRinger\Crowdin\Xclass\LanguageServiceFactoryXclassed::class,
+    ];
 };
 
 $boot();
