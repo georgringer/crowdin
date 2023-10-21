376551c51fdb7f39e7c1b475bc8a8baf92452f54
80Quattro <rr.jania@gmail.com>
2023-10-21 18:11:20 +0200

    [TASK] Vesrion up, drop v11 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Georg Ringer',
     'author_email' => '',
     'state' => 'beta',
-    'version' => '2.0.1',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '12.4.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
