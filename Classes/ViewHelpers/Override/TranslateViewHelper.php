9d67016ea5d144ccbb563cd364ee80cd2bac94ad
Georg Ringer <georg.ringer@gmail.com>
2024-01-18 09:03:06 +0100

    [TASK] WIP v12?

--- Classes/ViewHelpers/Override/TranslateViewHelper.php
+++ Classes/ViewHelpers/Override/TranslateViewHelper.php
@@ -6,12 +6,40 @@ namespace GeorgRinger\Crowdin\ViewHelpers\Override;
 
 use GeorgRinger\Crowdin\ExtensionConfiguration;
 use GeorgRinger\Crowdin\Xclass\LanguageServiceXclassed;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Http\ApplicationType;
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Mvc\RequestInterface as ExtbaseRequestInterface;
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3Fluid\Fluid\Core\ViewHelper\Exception;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 
-class TranslateViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\TranslateViewHelper
+class TranslateViewHelper extends AbstractViewHelper
 {
+
+    use CompileWithRenderStatic;
+
+    /**
+     * Output is escaped already. We must not escape children, to avoid double encoding.
+     *
+     * @var bool
+     */
+    protected $escapeChildren = false;
+
+    public function initializeArguments(): void
+    {
+        $this->registerArgument('key', 'string', 'Translation Key');
+        $this->registerArgument('id', 'string', 'Translation ID. Same as key.');
+        $this->registerArgument('default', 'string', 'If the given locallang key could not be found, this value is used. If this argument is not set, child nodes will be used to render the default');
+        $this->registerArgument('arguments', 'array', 'Arguments to be replaced in the resulting string');
+        $this->registerArgument('extensionName', 'string', 'UpperCamelCased extension key (for example BlogExample)');
+        $this->registerArgument('languageKey', 'string', 'Language key ("da" for example) or "default" to use. If empty, use current language. Ignored in non-extbase context.');
+        $this->registerArgument('alternativeLanguageKeys', 'array', 'Alternative language keys if no translation does exist. Ignored in non-extbase context.');
+    }
     /** @var ExtensionConfiguration */
     protected static $extensionConfiguration;
 
@@ -44,20 +72,55 @@ class TranslateViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\TranslateViewHelp
         }
 
         $request = $renderingContext->getRequest();
-        $extensionName = $extensionName ?? $request->getControllerExtensionName();
 
+        if (!$request instanceof ExtbaseRequestInterface) {
+            // Straight resolving via core LanguageService in non-extbase context
+            if (!str_starts_with($id, 'LLL:EXT:') && empty($default)) {
+                // Resolve "short key" without LLL:EXT: syntax given, if an extension name is given.
+                // @todo: We could consider to deprecate this case. It is mostly implemented for a more
+                //        smooth transition when (backend) controllers no longer feed an extbase request.
+                if (empty($extensionName)) {
+                    throw new \RuntimeException(
+                        'ViewHelper f:translate in non-extbase context needs attribute "extensionName" to resolve'
+                        . ' key="' . $id . '" without path. Either set attribute "extensionName" together with the short'
+                        . ' key "yourKey" to result in a lookup "LLL:EXT:your_extension/Resources/Private/Language/locallang.xlf:yourKey",'
+                        . ' or (better) use a full LLL reference like key="LLL:EXT:your_extension/Resources/Private/Language/yourFile.xlf:yourKey"',
+                        1639828178
+                    );
+                }
+                $id = 'LLL:EXT:' . GeneralUtility::camelCaseToLowerCaseUnderscored($extensionName) . '/Resources/Private/Language/locallang.xlf:' . $id;
+            }
+            $value = self::getLanguageService($request)->sL($id);
+            if (empty($value) || (!str_starts_with($id, 'LLL:EXT:') && $value === $id)) {
+                // In case $value is empty (LLL: could not be resolved) or $value
+                // is the same as $id and is no "LLL:", fall back to the default.
+                $value = $default;
+            }
+            if (!empty($translateArguments)) {
+                $value = vsprintf($value, $translateArguments);
+            }
+            return $value;
+        }
+
+        /** @var ExtbaseRequestInterface $request */
+        $extensionName = $extensionName ?? $request->getControllerExtensionName();
         try {
-            $value = static::translate($id, $extensionName, $translateArguments, $arguments['languageKey'], $arguments['alternativeLanguageKeys']);
+            // Trigger full extbase magic: "<f:translate key="key1" />" will look up
+            // "LLL:EXT:current_extension/Resources/Private/Language/locallang.xlf:key1" AND
+            // overloads from _LOCAL_LANG extbase TypoScript settings if specified.
+            // Not this triggers TypoScript parsing via extbase ConfigurationManager
+            // and should be avoided in backend context!
+            $value = LocalizationUtility::translate($id, $extensionName, $translateArguments, $arguments['languageKey'], $arguments['alternativeLanguageKeys'] ?? []);
         } catch (\InvalidArgumentException $e) {
+            // @todo: Switch to more specific Exceptions here - for instance those thrown when a package was not found, see #95957
             $value = null;
         }
         if ($value === null) {
-            $value = $default ?? $renderChildrenClosure();
+            $value = $default;
             if (!empty($translateArguments)) {
                 $value = vsprintf($value, $translateArguments);
             }
         }
-
         return $value;
     }
 
@@ -88,5 +151,21 @@ class TranslateViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\TranslateViewHelp
         }
 
         return parent::translate($id, $extensionName, $arguments, $languageKey, $alternativeLanguageKeys);
+    }
+
+
+    protected static function getLanguageService(ServerRequestInterface $request = null): LanguageService
+    {
+        if (isset($GLOBALS['LANG'])) {
+            return $GLOBALS['LANG'];
+        }
+        $languageServiceFactory = GeneralUtility::makeInstance(LanguageServiceFactory::class);
+        if ($request !== null && ApplicationType::fromRequest($request)->isFrontend()) {
+            $GLOBALS['LANG'] = $languageServiceFactory->createFromSiteLanguage($request->getAttribute('language')
+                ?? $request->getAttribute('site')->getDefaultLanguage());
+            return $GLOBALS['LANG'];
+        }
+        $GLOBALS['LANG'] = $languageServiceFactory->createFromUserPreferences($GLOBALS['BE_USER'] ?? null);
+        return $GLOBALS['LANG'];
     }
 }
--- Classes/Xclass/LanguageServiceFactoryXclassed.php
+++ Classes/Xclass/LanguageServiceFactoryXclassed.php
@@ -4,24 +4,58 @@ declare(strict_types=1);
 
 namespace GeorgRinger\Crowdin\Xclass;
 
+use TYPO3\CMS\Core\Authentication\AbstractUserAuthentication;
+use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
 use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
+use TYPO3\CMS\Core\Localization\Locale;
+use TYPO3\CMS\Core\Localization\Locales;
+use TYPO3\CMS\Core\Localization\LocalizationFactory;
+use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-class LanguageServiceFactoryXclassed extends LanguageServiceFactory
+class LanguageServiceFactoryXclassed
 {
+
+    protected Locales $locales;
+    protected LocalizationFactory $localizationFactory;
+    protected FrontendInterface $runtimeCache;
+
+    public function __construct(
+        Locales $locales,
+        LocalizationFactory $localizationFactory,
+        FrontendInterface $runtimeCache
+    ) {
+        $this->locales = $locales;
+        $this->localizationFactory = $localizationFactory;
+        $this->runtimeCache = $runtimeCache;
+    }
+
     /**
      * Factory method to create a language service object.
      *
-     * @param string $locale the locale (= the TYPO3-internal locale given)
-     *
-     * @return LanguageService
+     * @param Locale|string $locale the locale
      */
-    public function create(string $locale): LanguageService
+    public function create(Locale|string $locale): LanguageService
     {
-        $obj = GeneralUtility::makeInstance(LanguageServiceXclassed::class, $this->locales, $this->localizationFactory, $this->runtimeCache);
-        $obj->init($locale);
-
+        $obj = new LanguageServiceXclassed($this->locales, $this->localizationFactory, $this->runtimeCache);
+        $obj->init($locale instanceof Locale ? $locale : $this->locales->createLocale($locale));
         return $obj;
+    }
+
+    public function createFromUserPreferences(?AbstractUserAuthentication $user): LanguageService
+    {
+        if ($user && ($user->user['lang'] ?? false)) {
+            return $this->create($this->locales->createLocale($user->user['lang']));
+        }
+        return $this->create('en');
+    }
+
+    public function createFromSiteLanguage(SiteLanguage $language): LanguageService
+    {
+        $languageService = $this->create($language->getLocale() ?: $language->getTypo3Language());
+        // Always disable debugging for frontend
+        $languageService->debugKey = false;
+        return $languageService;
     }
 }
