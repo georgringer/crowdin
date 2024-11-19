ea1f9769dff9539258d9ff392834da8173747822
Xavier Perseguers <xavier@causal.ch>
2024-11-19 17:15:53 +0100

    [TASK] Configure Content Security Policy

---
+++ Configuration/ContentSecurityPolicies.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Directive;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Mutation;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\MutationCollection;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\MutationMode;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Scope;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\SourceScheme;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\UriValue;
+use TYPO3\CMS\Core\Type\Map;
+
+return Map::fromEntries([
+    // Provide declarations for the backend
+    Scope::backend(),
+
+    new MutationCollection(
+        new Mutation(
+            MutationMode::Append,
+            Directive::ScriptSrc,
+            new UriValue('https://cdn.crowdin.com')
+        ),
+        new Mutation(
+            MutationMode::Append,
+            Directive::ImgSrc,
+            new UriValue('https://cdn.crowdin.com')
+        ),
+        new Mutation(
+            MutationMode::Append,
+            Directive::ImgSrc,
+            new UriValue('https://crowdin-static.downloads.crowdin.com')
+        ),
+        new Mutation(
+            MutationMode::Append,
+            Directive::StyleSrc,
+            new UriValue('https://cdn.crowdin.com')
+        ),
+        new Mutation(
+            MutationMode::Append,
+            Directive::FrameSrc,
+            new UriValue('https://crowdin.com')
+        ),
+        new Mutation(
+            MutationMode::Append,
+            Directive::StyleSrc,
+            new UriValue('https://fonts.googleapis.com')
+        ),
+        new Mutation(
+            MutationMode::Extend,
+            Directive::FontSrc,
+            new UriValue('https://fonts.gstatic.com')
+        )
+    )
+]);
